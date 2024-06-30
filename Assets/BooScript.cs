08975918bc594fe90657d4acf2a26bccc27e1214
Ben Geo Abraham <bengeoabraham@gmail.com>
2024-06-30 23:14:22 +0530

    Death on collision and below Screen

--- Assets/BooScript.cs
+++ Assets/BooScript.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BooScript : MonoBehaviour
 {
 
     public Rigidbody2D boo;
     public float flightStrength;
+
+    [SerializeField] private Transform poleCheck;
+    [SerializeField] private LayerMask poleLayer;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        killReset();
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +26,28 @@ public class BooScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)== true){
+        if (Input.GetKeyDown(KeyCode.Space)){
 
             boo.velocity = Vector2.up * flightStrength;
         }
+        if (isTouching())
+        {
+            Debug.Log("YES");
+        }
+        if (transform.position.y < -20)
+        {
+            killReset() ;
+        }
             
+    }
+    private bool isTouching()
+    {
+        return Physics2D.OverlapCircle(poleCheck.position, 0.2f, poleLayer);
+    }
+    private void killReset()
+    {
+        Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
